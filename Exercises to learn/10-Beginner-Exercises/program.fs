016d2892b8879a4a7e25c67e8536d0aa793562b0
Christoffer Grünberg <gryn@itu.dk>
2024-12-24 13:25:17 +0100

    Created func for squareing lists

---
+++ Exercises to learn/10-Beginner-Exercises/program.fs
@@ -0,0 +1,11 @@
+﻿module Program
+
+open _10_Beginner_Exercises.SquareList
+open _10_Beginner_Exercises.Factorial
+
+[<EntryPoint>]
+let main argv =
+    //let squaredNumbers = SquareList.square [1; 2; 3; 4]
+    //printfn "%A" squaredNumbers
+    
+    0 // Return an integer exit code
---
+++ Exercises to learn/10-Beginner-Exercises/squareList.fs
@@ -0,0 +1,8 @@
+﻿module _10_Beginner_Exercises.SquareList
+
+open Microsoft.FSharp.Core
+
+module SquareList =
+    let square (numbers: int list): int list =
+        numbers |> List.map(fun n -> n * n)
+        
