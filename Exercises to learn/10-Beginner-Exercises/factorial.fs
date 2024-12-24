4e340dd2fc4d39d7368b322da5efedba3523205c
Christoffer Grünberg <gryn@itu.dk>
2024-12-24 13:25:26 +0100

    Created func for factorial

---
+++ Exercises to learn/10-Beginner-Exercises/factorial.fs
@@ -0,0 +1,12 @@
+﻿module _10_Beginner_Exercises.Factorial
+
+open Microsoft.FSharp.Core
+
+module Factorial =
+    let rec private factorialHelper (number: int) (value: int64) : int64 =
+        if number > 1 then
+            factorialHelper (number - 1) (value * int64 number)
+        else
+            (value * int64 number)
+
+    let factorial (number: int) : int64 = factorialHelper number 1
--- Exercises to learn/10-Beginner-Exercises/program.fs
+++ Exercises to learn/10-Beginner-Exercises/program.fs
@@ -8,4 +8,6 @@ let main argv =
     //let squaredNumbers = SquareList.square [1; 2; 3; 4]
     //printfn "%A" squaredNumbers
     
+    //let factorial = Factorial.factorial 13
+    //printfn "%A" factorial
     0 // Return an integer exit code
