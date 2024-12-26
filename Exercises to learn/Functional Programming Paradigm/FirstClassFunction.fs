7bf4b69f9c863da3f667a77e7d9e18ea6a512b8f
Christoffer Grünberg <gryn@itu.dk>
2024-12-26 12:39:05 +0100

    First-Class Function

---
+++ Exercises to learn/Functional Programming Paradigm/FirstClassFunction.fs
@@ -0,0 +1,8 @@
+﻿module Functional_Programming_Paradigm.FirstClassFunction
+
+//'a defines the input and output type,
+//making the function work with any type of input
+//as long as it is the same the function uses
+module FirstClassFunction =
+    let applyTwice (f: 'a -> 'a) (x: 'a): 'a =
+        f (f x)
--- Exercises to learn/Functional Programming Paradigm/Program.fs
+++ Exercises to learn/Functional Programming Paradigm/Program.fs
@@ -1,2 +1,8 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+
+open Functional_Programming_Paradigm.FirstClassFunction
+
+let f (x: int) = x + 2
+printfn "%A" (FirstClassFunction.applyTwice f 2)
+printfn "%A" (FirstClassFunction.applyTwice (fun x -> x * 2) 2)
+printfn "%A" (FirstClassFunction.applyTwice (fun x -> x + " ha") "b")
