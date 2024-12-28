176676bd5837ab6373b676cf7df7fb0cc3b3fe07
Christoffer Grünberg <gryn@itu.dk>
2024-12-28 09:58:23 +0100

    Type Inference and Polymorphism

--- Exercises to learn/Functional Programming Paradigm/Program.fs
+++ Exercises to learn/Functional Programming Paradigm/Program.fs
@@ -2,13 +2,21 @@
 
 open Functional_Programming_Paradigm.FirstClassFunction
 open Functional_Programming_Paradigm.HigherOrderFunctions
+open Functional_Programming_Paradigm.TypeInferenceAndPolymorphism
+open Microsoft.FSharp.Core
 
 // let f1 (x: int) = x + 2
 // printfn "%A" (FirstClassFunction.applyTwice f1 2)
 // printfn "%A" (FirstClassFunction.applyTwice (fun x -> x * 2) 2)
 // printfn "%A" (FirstClassFunction.applyTwice (fun x -> x + " ha") "b")
 
-let filter (x: int): bool = x % 2 = 0
-let map (x: int): int = x * 2
-let list = [1; 2; 3; 4]
-printf "%A" (HigherOrderFunctions.filterMap list filter map)
+// let filter (x: int): bool = x % 2 = 0
+// let map (x: int): int = x * 2
+// let list = [1; 2; 3; 4]
+// printf "%A" (HigherOrderFunctions.filterMap list filter map)
+
+let predicate (x: string) : bool = x.Contains 'e'
+let predicate2 (x: string) : bool = x.Contains 'q'
+let words = [ "hi"; "boo"; "hello"; "nik"; "hey" ]
+printfn "%A" (TypeInferenceAndPolymorphism.findFirst words predicate)
+printfn "%A" (TypeInferenceAndPolymorphism.findFirst words predicate2)
---
+++ Exercises to learn/Functional Programming Paradigm/TypeInferenceAndPolymorphism.fs
@@ -0,0 +1,5 @@
+﻿module Functional_Programming_Paradigm.TypeInferenceAndPolymorphism
+
+module TypeInferenceAndPolymorphism =
+    let findFirst (list: 'a list) (predicate: 'a -> bool): 'a option =
+        list |> List.tryFind predicate
